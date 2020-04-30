480e5dcf12802f06001b99386be94ba5693aad1a
Inga <inga.dukstyna@gmail.com>
2020-04-30 21:54:33 +0300

    Optimized code. Conditional formatting of Order Status cells. A few style changes. Check for existing user name. Default selection of check box.

--- src/plan/model/plan.sql
+++ src/plan/model/plan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2020 m. Kov 30 d. 17:25
+-- Generation Time: 2020 m. Bal 30 d. 20:39
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -45,19 +45,36 @@ CREATE TABLE `plan` (
 
 INSERT INTO `plan` (`id`, `order_number`, `product_type`, `worker`, `planned_time`, `actual_time`, `order_status`, `user_id`) VALUES
 (1, 9001501, 'Curtainsider', 'K.Samas', 3, 4, 'Completed', 1),
-(2, 9001502, 'Truck Box Body', 'K.Samas', 3, 0, 'Sheduled', 2),
+(2, 9001502, 'Truck Box Body', 'K.Samas', 3, 0, 'Scheduled', 2),
 (3, 9001503, 'Semitrailer', 'K.Samas, S.Saulaitis', 2, 0, 'Scheduled', 2),
-(4, 9001504, 'Truck Box Body', 'M.Morkuvienė, S.Saulaitis', 2, 2, 'Completed', 2),
+(4, 9001504, 'Truck Box Body', 'M.Morkuvienė, S.Saulaitis', 2, 3, 'Completed', 2),
 (5, 9001505, 'Curtainsider', 'K.Samas, M.Morkuvienė', 2, 0, 'Scheduled', 2),
 (6, 9001506, 'Semitrailer', 'K.Samas, M.Morkuvienė', 2, 0, 'Processing', 2),
 (8, 9001510, 'Drawbar Trailer', 'A.Adomaitienė, S.Saulaitis', 0, 0, 'Scheduled', 1),
 (9, 9001508, 'Semitrailer', 'J.Jonaitis, S.Saulaitis', 3, 0, 'Scheduled', 1),
-(10, 9001509, 'Drawbar Trailer', 'P.Petraitis, S.Saulaitis', 11, 0, 'Scheduled', 1),
 (11, 9001513, 'Semitrailer', 'K.Samas, S.Saulaitis', 2, 0, 'Scheduled', 1),
 (12, 9001511, 'Curtainsider', 'J.Jonaitis, K.Samas', 3, 2, 'Completed', 2),
-(13, 9001512, 'Drawbar Trailer', 'K.Samas, P.Petraitis', 6, 5, 'Processing', 2),
-(14, 9001514, 'Truck Box Body', 'K.Samas, S.Saulaitis', 5, 6, 'Scheduled', 2),
-(15, 9001515, 'Semitrailer', 'K.Samas, A.Adomaitienė', 2, 2, 'Scheduled', 2);
+(13, 9001512, 'Drawbar Trailer', 'K.Samas, P.Petraitis', 6, 0, 'Processing', 2),
+(14, 9001514, 'Truck Box Body', 'K.Samas, S.Saulaitis', 5, 6, 'Completed', 2),
+(15, 9001515, 'Semitrailer', 'K.Samas, A.Adomaitienė', 2, 2, 'Completed', 2),
+(17, 9001517, 'Curtainsider', 'J.Jonaitis, K.Samas', 5, 0, 'Processing', 2),
+(18, 9001520, 'Truck Box Body', 'M.Morkuvienė, S.Saulaitis', 5, 0, 'Scheduled', 1),
+(20, 9001519, 'Drawbar Trailer', 'J.Jonaitis, S.Saulaitis', 3, 0, 'Scheduled', 1),
+(21, 9001522, 'Truck Box Body', 'K.Samas, A.Adomaitienė', 2, 0, 'Scheduled', 2),
+(22, 9001518, 'Semitrailer', 'K.Samas, A.Adomaitienė', 2, 0, 'Scheduled', 2),
+(24, 9001523, 'Curtainsider', 'J.Jonaitis, A.Adomaitienė', 2, 0, 'Processing', 3),
+(25, 9001524, 'Completed', 'M.Morkuvienė, A.Adomaitienė', 1, 2, 'Completed', 3),
+(26, 9001527, 'Truck Box Body', 'M.Morkuvienė, P.Petraitis, A.Adomaitienė', 5, 0, 'Scheduled', 3),
+(27, 9001528, 'Curtainsider', 'A.Adomaitienė, S.Saulaitis', 2, 0, 'Scheduled', 3),
+(28, 9001522, 'Drawbar Trailer', 'A.Adomaitienė', 3, 0, 'Processing', 3),
+(30, 9001525, 'Truck Box Body', 'J.Jonaitis, M.Morkuvienė, P.Petraitis', 2, 1, 'Completed', 4),
+(31, 9001526, 'Drawbar Trailer', 'M.Morkuvienė', 1, 0, 'Scheduled', 4),
+(32, 9001530, 'Semitrailer', 'M.Morkuvienė, P.Petraitis', 3, 0, 'Processing', 4),
+(33, 9001533, 'Truck Box Body', 'M.Morkuvienė', 3, 0, 'Scheduled', 4),
+(34, 9001531, 'Semitrailer', 'J.Jonaitis, A.Adomaitienė', 3, 2, 'Completed', 6),
+(35, 9001535, 'Curtainsider', 'J.Jonaitis', 2, 0, 'Processing', 6),
+(36, 9001536, 'Truck Box Body', 'J.Jonaitis, K.Samas', 1, 0, 'Scheduled', 6),
+(37, 9001537, 'Drawbar Trailer', 'J.Jonaitis, K.Samas', 3, 0, 'Scheduled', 6);
 
 --
 -- Indexes for dumped tables
@@ -77,7 +94,7 @@ ALTER TABLE `plan`
 -- AUTO_INCREMENT for table `plan`
 --
 ALTER TABLE `plan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
